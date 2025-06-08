3dfbef33edb39b222daf1b45c012d0aced40999b
barts <bartsteensma@outlook.com>
2025-06-08 13:20:49 +0200

    feat: add user commands for saturation and light delta control

---
+++ lua/saturate/_usercommands.lua
@@ -0,0 +1,59 @@
+local M = {}
+local commands = require("saturate.commands")
+
+--- Create user commands for saturate.nvim
+function M.setup()
+    -- IncrementSaturation command
+    vim.api.nvim_create_user_command('IncrementSaturation', function(opts)
+        local step = nil
+        if opts.args ~= "" then
+            step = tonumber(opts.args)
+            if not step then
+                vim.notify("Invalid step: must be a number", vim.log.levels.ERROR)
+                return
+            end
+        end
+        commands.increment_saturation(nil, step)
+    end, { nargs = '?', desc = "Increment saturation by step (or default step if not provided)" })
+
+    -- DecrementSaturation command
+    vim.api.nvim_create_user_command('DecrementSaturation', function(opts)
+        local step = nil
+        if opts.args ~= "" then
+            step = tonumber(opts.args)
+            if not step then
+                vim.notify("Invalid step: must be a number", vim.log.levels.ERROR)
+                return
+            end
+        end
+        commands.decrement_saturation(nil, step)
+    end, { nargs = '?', desc = "Decrement saturation by step (or default step if not provided)" })
+
+    -- IncrementLightDelta command
+    vim.api.nvim_create_user_command('IncrementLightDelta', function(opts)
+        local step = nil
+        if opts.args ~= "" then
+            step = tonumber(opts.args)
+            if not step then
+                vim.notify("Invalid step: must be a number", vim.log.levels.ERROR)
+                return
+            end
+        end
+        commands.increment_light_delta(nil, step)
+    end, { nargs = '?', desc = "Increment light delta by step (or default step if not provided)" })
+
+    -- DecrementLightDelta command
+    vim.api.nvim_create_user_command('DecrementLightDelta', function(opts)
+        local step = nil
+        if opts.args ~= "" then
+            step = tonumber(opts.args)
+            if not step then
+                vim.notify("Invalid step: must be a number", vim.log.levels.ERROR)
+                return
+            end
+        end
+        commands.decrement_light_delta(nil, step)
+    end, { nargs = '?', desc = "Decrement light delta by step (or default step if not provided)" })
+end
+
+return M
--- lua/saturate/init.lua
+++ lua/saturate/init.lua
@@ -1,11 +1,13 @@
 local M = require("saturate._boosters")
 local opts = require("saturate.opts")
+local usercommands = require("saturate._usercommands")
 
 --- Setup the nvim-saturate plugin with user options.
 ---@param user_opts saturate.Opts? (optional) User settings.
 function M.setup(user_opts)
   user_opts = user_opts or {}
   opts.update(user_opts)
+  usercommands.setup()
 end
 
 return M
